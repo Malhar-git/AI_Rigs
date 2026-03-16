def88383c49b34ec00017bffa97bb5c39c58df72
Malhar1080 <malhar1080p@gmail.com>
2026-03-16 22:00:17 +0530

    add css global design system

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {DM_Sans, JetBrains_Mono} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jetBrainsMono = JetBrains_Mono({
+  variable: "--font-jetBrains-mono",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dmSans.variable} ${jetBrainsMono.variable} antialiased`}
       >
         {children}
       </body>
